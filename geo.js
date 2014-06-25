34f5a118ac5b057801ed27f23cb793112229fde0
rugbyprof <terry.griffin@mwsu.edu>
2014-06-25 04:30:36 -0500

    fixing geo.js

--- geo.js
+++ geo.js
@@ -44,65 +44,49 @@ function initialize() {
         $.post("class.geojson.php", PostData)
             .done(function( data ) {
                 //data = JSON.parse(data);
+                deleteMarkers();
                 var obj = data.features;
                 for (var i in obj){
-                    traverse(obj[i],process);
-                    console.log(obj[i].geometry.type);
-                    console.log(obj[i]);
-                    console.log("----------------------------------------");
+                    switch(obj[i].geometry.type){
+                        case "MultiPolygon":
+                            addMultiPolygon(obj[i].geometry);
+                            break;
+                        case "Polygon":
+                            addPolygon(obj[i].geometry);
+                            break;
+                        case "Line":
+                            addLine(obj[i].geometry);
+                            break;
+                        case "Point":
+                            addPoint(obj[i].geometry);
+                            break:
+                    }
                 }
-                //console.log(data.Poly.length);
-                //deleteMarkers();
-                //addPolygon(data.Poly);
             });
     });  
 
 }
 
-//function traverse(jsonObj) {
-//    if( typeof jsonObj == "object" ) {
-//        if(jsonObj !== null){
-//            $.each(jsonObj, function(k,v) {
-//                // k is either an array index or object key
-//                traverse(v);
-//            });
-//        }
-//    }
-//    else {
-//        // jsonOb is a number or string
-//        console.log(jsonObj);
-//    }
-//}
-
-function process(key,value) {
+function addMultiPolygon(obj){
     
-    //if(isInt(key) === true)
-        console.log(key + " : "+value);
-}
-
-function traverse(o,func) {
-    for (var i in o) {
-        func.apply(this,[i,o[i]]);
-        if (o[i] !== null && typeof(o[i])=="object") {  
-            //going on step down in the object tree!!
-            traverse(o[i],func);
-        }
+    color = HexColor();
+    
+    for(var i=0;i<obj.length;i++){
+        addPolygon(obj,color);
     }
+    
 }
 
-function isInt(value) { 
-    return !isNaN(parseInt(value,10)) && (parseFloat(value,10) == parseInt(value,10)); 
-}
-
-function addPolygon(obj) {
+function addPolygon(obj,c) {
+    
+    color = typeof c !== 'undefined' ? c : HexColor();
+    
     var PolyCoords = [];
-
 
     for(i=0;i<obj.length;i++){
         var latlng = new google.maps.LatLng(obj[i][1],obj[i][0]);
         PolyCoords.push(latlng);
     }
-
 
     var polygon = new google.maps.Polygon({
         paths: PolyCoords,
@@ -115,9 +99,10 @@ function addPolygon(obj) {
     });
 
     polygons.push(polygon); //Add polygon to global array of polygons
+    
 }
 
-function addMarker(obj) {
+function addPoint(obj) {
     marker = new google.maps.Marker({
         position: new google.maps.LatLng(obj.latitude,obj.longitude),
         title: obj.fullname,
@@ -151,3 +136,25 @@ function deleteMarkers() {
     polygons = [];
 }
 
+function HexColor(){
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+function process(key,value) {
+    //if(isInt(key) === true)
+        console.log(key + " : "+value);
+}
+
+function traverse(o,func) {
+    for (var i in o) {
+        func.apply(this,[i,o[i]]);
+        if (o[i] !== null && typeof(o[i])=="object") {  
+            //going on step down in the object tree!!
+            traverse(o[i],func);
+        }
+    }
+}
+
+function isInt(value) { 
+    return !isNaN(parseInt(value,10)) && (parseFloat(value,10) == parseInt(value,10)); 
+}
