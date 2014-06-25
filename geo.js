8fdd77746d2f2ccd2f27a46bcca8395b070d58ae
rugbyprof <terry.griffin@mwsu.edu>
2014-06-25 02:15:16 -0500

    fixing geo.js

--- geo.js
+++ geo.js
@@ -74,7 +74,7 @@ function process(key,value) {
 
 function traverse(o,func) {
     for (var i in o) {
-        if(this !== null && !isInt(this))
+        if(this !== null && isInt(this) !== true)
             func.apply(this,[i,o[i]]);  
         if (o[i] !== null && typeof(o[i])=="object") {
             //going on step down in the object tree!!
