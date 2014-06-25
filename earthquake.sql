8cec431e95280e581faafabe171a6c42485b0839
rugbyprof <terry.griffin@mwsu.edu>
2014-06-25 10:46:05 -0500

    adding query

--- earthquake.sql
+++ earthquake.sql
@@ -1,4 +1,4 @@
 SELECT ASTEXT( A.SHAPE ) , location, state
 FROM  `earth_quakes` A,  `state_borders` B
-WHERE CONTAINS( B.SHAPE, A.SHAPE ) 
+WHERE CONTAINS( A.SHAPE, B.SHAPE ) 
 AND state =  'Texas'
